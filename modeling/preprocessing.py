dafe654ce344a6e7a01cd192aea773bccbc30b41
Anton Roman Young <89807629+antonryoung02@users.noreply.github.com>
2023-12-22 17:26:15 -0700

    new preprocess_data passes tests

--- modeling/preprocessing.py
+++ modeling/preprocessing.py
@@ -4,15 +4,50 @@ from PIL import Image
 import os
 
         
-def preprocess_image(image:Image, width:int, height:int) -> Image:
-    image = image.resize(width, height)
-    return image.convert("RGB")
+def preprocess_image(image: Image, dimensions: tuple) -> Image:
+    """
+    Converts an Image object to desired size / #channels
+    """
 
-def preprocess_data(input_dir:str, output_dir:str, dimensions:tuple):
-    return None
+    width, height = dimensions
+    resized_image = image.resize((width, height))
+    return resized_image.convert("RGB")
 
+def preprocess_data(input_dir:str, output_dir:str, dimensions:tuple=(320,180)) -> None:
+    """
+    Transforms all .png files to desired specifications using preprocess_image
 
+    param input_dir: the directory of untransformed images
+    param output_dir: where transformed images are stored
+    param dimensions: (width, height) in pixels of transformed image size
+    """
 
-#Load all files
+    if not os.path.exists(output_dir):
+        return
+    if not os.path.exists(input_dir):
+        return
+
+    for filename in os.listdir(input_dir):
+        if filename.lower().endswith('.png'):
+            image_path = os.path.join(input_dir, filename)
+            image = Image.open(image_path)
+            processed_image = preprocess_image(image, dimensions)
+            
+            output_path = os.path.join(output_dir, filename)
+            processed_image.save(output_path)
+
+def process_training_data():
+    """
+    Function that processes both my pos and neg data
+    """
+    pos_input = "./data_pos"
+    pos_output = "./processed_data/data_pos"
+    neg_input = "./data_neg"
+    neg_output = "./processed_data/data_neg"
+
+    preprocess_data(pos_input, pos_output)
+    preprocess_data(neg_input, neg_output)
+
+process_training_data()
 
 
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,18 +1,26 @@
 from modeling.preprocessing import preprocess_image, preprocess_data
 from PIL import Image
 import os
+def clear_directory(directory):
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f'Error: {e}')
 
 def test_preprocess_data():
     input_dir = "./tests/test_input"
     output_dir = "./tests/test_output"
-
     width, height = 300, 200
+
+    clear_directory(output_dir)
     preprocess_data(input_dir, output_dir, (width, height))
-
     output_files = os.listdir(output_dir)
-    print(type(output_files))
 
-    assert len(output_files) == 5
+    assert len(output_files) == 7
 
     for file in output_files:
         image = Image.open(os.path.join(output_dir, file))
